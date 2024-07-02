f2b1c8212b8e98495c3be1204df19c64c7617a19
AmeerBadir <ameer.badir123@gmail.com>
2024-07-02 19:32:33 +0300

    add polybot

--- polybot/app.py
+++ polybot/app.py
@@ -1,13 +1,39 @@
+import json
+
 import flask
 from flask import request
 import os
 from bot import ObjectDetectionBot
+import boto3
+from botocore.exceptions import ClientError
+
+
+def get_secret():
+    secret_name = "ameer-tel-token"
+    region_name = "us-east-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+
+    secret = json.loads(get_secret_value_response['SecretString'])['TELEGRAM_TOKEN']
+    return secret
+
 
 app = flask.Flask(__name__)
 
-
 # TODO load TELEGRAM_TOKEN value from Secret Manager
-TELEGRAM_TOKEN = ...
+TELEGRAM_TOKEN = get_secret()
 
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
@@ -28,11 +54,12 @@ def webhook():
 def results():
     prediction_id = request.args.get('predictionId')
 
+    dynamodb = boto3.resource('dynamodb')
     # TODO use the prediction_id to retrieve results from DynamoDB and send to the end-user
-
-    chat_id = ...
-    text_results = ...
-
+    dyanamo_db = dynamodb.Table("ameerbadir-aws")
+    response = dyanamo_db.get_item(key={"prediction_id": prediction_id})
+    chat_id = response['Item']['chatId']
+    text_results = response['Item']['results']
     bot.send_text(chat_id, text_results)
     return 'Ok'
 
@@ -46,5 +73,5 @@ def load_test():
 
 if __name__ == "__main__":
     bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL)
-
-    app.run(host='0.0.0.0', port=8443)
+    ssl_context = ("YOURPUBLIC.pem", "YOURPRIVATE.key")
+    app.run(host='0.0.0.0', port=8443, ssl_context=ssl_context)
--- polybot/bot.py
+++ polybot/bot.py
@@ -1,8 +1,10 @@
+import json
 import telebot
 from loguru import logger
 import os
 import time
 from telebot.types import InputFile
+import boto3
 
 
 class Bot:
@@ -17,7 +19,7 @@ class Bot:
         time.sleep(0.5)
 
         # set the webhook URL
-        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
+        self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60,certificate=open("YOURPUBLIC.pem", 'r'))
 
         logger.info(f'Telegram Bot information\n\n{self.telegram_bot_client.get_me()}')
 
@@ -64,14 +66,72 @@ class Bot:
         logger.info(f'Incoming message: {msg}')
         self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
 
+def get_result(respond):
+    if "labels" in respond:
+        try:
+            dict_objects = dict()
+            total_count = 0
+            for lab in respond['labels']:
+                item_class = lab['class']
+                if item_class in dict_objects:
+                    dict_objects[item_class] += 1
+                else:
+                    dict_objects[item_class] = 1
+                total_count += 1
+            return total_count, dict_objects
+        except Exception as e:
+            logger.error(f"Error in get_result function: {e}")
+            return None, {}
+def summary_msg(total_object_number, object_dictionary):
+    msg = f'We detect {total_object_number} objects.\n'
+    for key, val in object_dictionary.items:
+        object_count = f'object {key} found {val} times.\n'
+        msg += object_count
+    return msg
 
 class ObjectDetectionBot(Bot):
+    def send_job_to_sqs(self, img_name, chat_id):
+        try:
+            sqs = boto3.client('sqs')
+            queue_url = "https://sqs.us-east-1.amazonaws.com/700935310038/ameerbadir-sqs"
+            job_message = {
+                'image_key': img_name,
+                'chat_id': chat_id
+            }
+            respone = sqs.send_message(QueueUrl=queue_url, MessageBody=json.dumps(job_message))
+            return respone
+        except Exception as e:
+            logger.error(f"Sending job to SQS failed: {e}")
     def handle_message(self, msg):
         logger.info(f'Incoming message: {msg}')
+        if "text" in msg:
+            if msg["text"] == '/start':
+                self.send_text(msg['chat']['id'], 'Hi!, give a photo to start object detection')
 
+        # upload the photo to S3
         if self.is_current_msg_photo(msg):
             photo_path = self.download_user_photo(msg)
+            img_name = os.path.basename(photo_path)
+            s3 = boto3.client('s3')
+            images_bucket = os.environ['BUCKET_NAME']
 
-            # TODO upload the photo to S3
-            # TODO send a job to the SQS queue
-            # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
+            try:
+                s3.upload_file(photo_path, images_bucket, img_name)
+                self.send_text(msg['chat']['id'], "Image uploaded successfully")
+                #  send a job to the SQS queue
+                # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
+                self.send_text(msg['chat']['id'], "Your image is being processed. Please wait...")
+                response = self.send_job_to_sqs(img_name, msg['chat']['id'])
+                total_object_number, object_dictionary = get_result(response.json())
+                if total_object_number is None and object_dictionary == {}:
+                    self.send_text(msg['chat']['id'], "failed to upload image")
+                else:
+                    response_msg = summary_msg(total_object_number, object_dictionary)
+                    self.send_text(msg['chat']['id'], response_msg)
+
+
+            except Exception as e:
+                self.send_text(msg['chat']['id'], "failed to upload image")
+                logger.error(f"Upload image to s3 failed: {e}")
+
+
